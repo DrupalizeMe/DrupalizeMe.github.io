5984d2c6822721729836971782f24e4f7a5ed6b6
Joe Fender <drupaljoe@gmail.com>
2014-01-15 15:29:07 +0000

    playback speed button

--- videos/js/scripts.js
+++ videos/js/scripts.js
@@ -1,4 +1,52 @@
 $(function() {
+  // Add 2x speed button for HTML5
+  jwplayer("video").setup({
+    playlist: [{
+      image: "images/bg.png",
+      sources: [{
+        file: "https://s3.amazonaws.com/drupalize.me/_testing_joe_/jwp6/2013-10-31-Using-Drupal-4-01-960x540-300k.mp4",
+        label: "360p SD"
+      },{
+        file: "https://s3.amazonaws.com/drupalize.me/_testing_joe_/jwp6/2013-10-31-Using-Drupal-4-01-960x540-1200k.mp4",
+        label: "720p HD"
+      }]
+    }],
+    file: "http://developer.longtailvideo.com/trac/export/944/trunk/html5/test/files/bunny.mp4",
+    skin: "jwplayer/six/six.xml",
+    width: "100%",
+    aspectratio: "16:9"
+  });
+
+  jwplayer().onPlay(function(){
+    playerSetup(this);
+  });
+
+  function playerSetup(player) {
+    if (player.getRenderingMode() == 'html5') {
+      player.addButton(
+        "images/1x.png",
+        "Current Speed",
+        function() {
+          var video = document.getElementById('video');
+
+          if (video.querySelector("video").playbackRate == 1.0) {
+            video.querySelector("video").playbackRate = 1.5;
+            $('#video_dock_transcript').css('background-image', "url('images/15x.png')");
+          }
+          else if (video.querySelector("video").playbackRate == 1.5) {
+            video.querySelector("video").playbackRate = 2.0;
+            $('#video_dock_transcript').css('background-image', "url('images/2x.png')");
+          }
+          else {
+            video.querySelector("video").playbackRate = 1.0;
+            $('#video_dock_transcript').css('background-image', "url('images/1x.png')");
+          }
+        },
+        "transcript"
+      );
+    }
+  }
+
   $('.playlist-content a').click(function() {
     $('.playlist-content a').removeClass('active');
     $(this).addClass('active');
