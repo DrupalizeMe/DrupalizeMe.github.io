1f4baf06e04becd75db846869a88deea9621ff2a
Joe Fender <drupaljoe@gmail.com>
2014-01-16 18:05:39 +0000

    updates icons

--- videos/js/scripts.js
+++ videos/js/scripts.js
@@ -1,5 +1,14 @@
 $(function() {
 
+  // Store our player icons as base64 icons to improve performance.
+  var transcript_icon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAUCAYAAABiS3YzAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAN5JREFUeNpi/P//PwMQKEAxseADEF/AKQs0NOA/eWAByEHYMIg48J98oIDNUBYsjp8I9R42UADE/Eh8UJA9gLINoOwP6C7F6SUodkBzqQNU3ACIPwDxBSAWQHdpPNSVuFwagEcc5AN9kIuxeT+fgULAxEADgO7Sj0CcgMf7E6BeJMlQkKYNeNSDYn8/qYbWA7EAgSRFsveHT0QlQNMyOjgADTqQ/gssWCTxRZQCHkMPQukP6Ib6Q4s0XC61x2NhAEwftojSJzMoP1Aroh7giv0JZBq4EJehjLSoTgACDAAM7A9aPeB9aAAAAABJRU5ErkJggg==";
+  var speed_normal_icon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAUCAYAAABiS3YzAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAONJREFUeNq8VYsRgjAMDSwAG8gIjuAKbuAIdQJxE9zADewIdQNGqBPE4AWt2O+1+O4e9Er6SPpKAEQEC1uiIEqixg80zwmOsa0F26RYCLmgOTYoOmA6hmXWuYIzrjZRgfl4b0U1pQ4AI7GBPDyIHVHXdDl4BO/ES6Row1qv8qWjHMUG7BK2QM7la0eme75Pz2/EM3HLc4p4cmRcQeDNvZHpSOyYo2cN1AlGbIxx6wus2bUYHNmIib030mPUXLIyTEDL+MeoUgf/6wNY7fDr4B7FoWetYg1l+GvrW61JF/mdPAUYAF/t/6lwFqcoAAAAAElFTkSuQmCC";
+  var speed_fast_icon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAUCAYAAABiS3YzAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAQ9JREFUeNq0lf0NgjAQxS/G/2UDcQM2sCPoBoygG3QE3YARcAJ1A5hANhAnqK96jedpgx/wkl+Aa3hc79pCzjmKkIINOLhntaAEeezdd8GEzT5RAxZdpt6wct/Lxkx/NXwxlqal+19GmhrXj3xTaUx35fTQBVRgLmI1aMVzBib0qvltjDNtxXLJOKanlfLVdNTejtVXLcXlZ2NACqZgxzNa8VjBPkbXsw11eZNpwtnKVZLxmJV1HalsJpEs9+AMTiABJcd95gfVExqpBmhtuXFrMANL0LBZzeaexdNbqlFh61lxr+MhVooyFOLeBtPC9acs1LSgfnS8rYght+lgB8ogR99gh7T+ndhffidXAQYA6cCTMaDN2rIAAAAASUVORK5CYII=";
+  var speed_fastest_icon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAUCAYAAABiS3YzAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAQZJREFUeNq0VdERgjAMLZz/sgFsYDcQN4ANYANHYARGcARGYAPLBOIGOEFN4H1UbDkR+u7e0bTpS5peg9BaCwclsSYq/YmeeCOmrr22yYTY6t/QIviiKDsMeh3Yv3CJ/iNoIrWJKr0NnFDEWqGYcCWexDYcifU4Qpa9IzrPN2uzDbiWpH23RM6JCuPI4WNDzseXjkU+yoPYQvxJ7Igl1kvYc0gWTRyiMb4VMYXNdR+IF6xZ7yFcOMYLJeiJjTGfIfvMtTHEJhskxAYIdAhUQbSC/YWDcRlzFDM7gi/X+gxRhbEJFYwvYMo2FtvBmSehcRl7YCqXj2fqvaF4aX3emvQuv5O3AAMAeQRq8MPwJF0AAAAASUVORK5CYII=";
+  var layout_normal_icon = "";
+  var layout_stacked_icon = "";
+
+
   // Embed JWPlayer
   jwplayer("video").setup({
     playlist: [{
@@ -21,7 +30,6 @@ $(function() {
         kind: "captions"
       }]
     }],
-    file: "http://developer.longtailvideo.com/trac/export/944/trunk/html5/test/files/bunny.mp4",
     skin: "jwplayer/skins/bekle.xml",
     width: "100%",
     aspectratio: "16:9"
@@ -36,22 +44,21 @@ $(function() {
     // Add 2x speed button when in HTML5 mode
     if (player.getRenderingMode() == 'html5') {
       player.addButton(
-        "images/1x.png",
+        speed_normal_icon,
         "Speed",
         function() {
           var video = document.getElementById('video').querySelector("video");
-
           if (video.playbackRate == 1.0) {
             video.playbackRate = 1.5;
-            $('#video_dock_speed').css('background-image', "url('images/15x.png')");
+            $('#video_dock_speed').css('background-image', "url(" + speed_fast_icon + ")");
           }
           else if (video.playbackRate == 1.5) {
             video.playbackRate = 2.0;
-            $('#video_dock_speed').css('background-image', "url('images/2x.png')");
+            $('#video_dock_speed').css('background-image', "url(" + speed_fastest_icon + ")");
           }
           else {
             video.playbackRate = 1.0;
-            $('#video_dock_speed').css('background-image', "url('images/1x.png')");
+            $('#video_dock_speed').css('background-image', "url(" + speed_normal_icon + ")");
           }
         },
         "speed"
@@ -60,7 +67,7 @@ $(function() {
 
     // Add a transcript toggle button
     player.addButton(
-      "images/1x.png",
+      transcript_icon,
       "Transcript",
       function() {
         $('.transcript').toggle();
@@ -70,10 +77,16 @@ $(function() {
 
     // Add a layout toggle button
     player.addButton(
-      "images/1x.png",
+      layout_stacked_icon,
       "Layout",
       function() {
         $('.hero').toggleClass('stacked');
+        if ($('.hero.stacked').length) {
+          $('#video_dock_layout').css('background-image', "url(" + layout_normal_icon + ")");
+        }
+        else {
+          $('#video_dock_layout').css('background-image', "url(" + layout_stacked_icon + ")");
+        }
       },
       "layout"
     );
