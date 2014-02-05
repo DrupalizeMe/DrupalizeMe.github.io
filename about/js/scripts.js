71e5133b2c558cea6bbd1b21b3bd6343e6418bd8
Joe Fender <drupaljoe@gmail.com>
2014-02-05 15:32:39 +0000

    adds fadein

--- about/js/scripts.js
+++ about/js/scripts.js
@@ -5,4 +5,9 @@ $(function() {
     delay: 150,
     speed: 200
   });
+
+  $(window).load(function() {
+    $('h2, .about--paragraph, .about--team-member, .about--team--banner, .about--offices-button').addClass('active');
+  });
+
 });
